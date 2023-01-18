a4a4becdc57d04ec8c54c634994be8d22bb54c0e
codeuiandy <frostandy41@gmail.com>
2023-01-18 14:11:18 +0100

    updates

--- app.js
+++ app.js
@@ -1,54 +1,24 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/test", (req, res, next) => {
-  console.log("MIDDLE wear");
-  res.send([
-    {
-      name: "andrew",
-      age: 20,
-      favColor: {
-        color: "red",
-      },
-    },
-    {
-      name: "Joe",
-      age: 20,
-      favColor: {
-        color: "red",
-      },
-    },
-    {
-      name: "Titi",
-      age: 20,
-      favColor: {
-        color: "red",
-      },
-    },
-    {
-      name: "Max",
-      age: 20,
-      favColor: {
-        color: "red",
-      },
-    },
-  ]);
-  // res.setHeader("Content-Type","j")
-  //  next(); // Allows the next middleware block to run
-});
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.use("/login", (req, res, next) => {
-  console.log("res>>>>>", req.body);
-  res.send({ response: req.body.title });
-});
-
-app.use("/", (req, res, next) => {
-  console.log("MIDDLE wear again ");
-  res.send({});
-});
+app.use(errorController.get404);
 
 app.listen(3000);
